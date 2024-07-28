af6e529645078381a39c01a2591524afe5dbb241
Aditya Ranjan Jha <adityaranjan2014@gmail.com>
2024-07-28 12:03:42 +0530

    added blog item component for the reusabilty in future for futher creating different categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+'use client'
+
 import Image from "next/image";
 import Header from "../components/Header";
+import BlogItem from "../components/BlogItem";
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <BlogItem/>
     </div>
         
   );
